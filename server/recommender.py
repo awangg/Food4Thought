f9ad3a7a10eebc337f479cd38846fcfb1c3c58a3
Sarah Han <swh3@rice.edu>
2021-02-06 01:21:18 -0600

    started recommender

---
+++ server/recommender.py
@@ -0,0 +1,54 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.corpus import stopwords 
+from nltk.tokenize import WordPunctTokenizer
+import warnings
+import string
+import nltk
+import ssl
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+nltk.download('stopwords')
+
+
+
+
+df = pd.read_csv('data/out.csv')
+df_business = pd.read_csv('data/places.csv')
+print(df.head())
+
+#Select only stars and text
+#replaced 'business_id', 'user_id', 'stars', 'text' with 
+bus_data = df[['gPlusUserId', 'reviewerName', 'rating', 'reviewText']]
+stop = []
+for word in stopwords.words('english'):
+    s = [char for char in word if char not in string.punctuation]
+    stop.append(''.join(s))
+
+
+def text_process(mess):
+    """
+    Takes in a string of text, then performs the following:
+    1. Remove all punctuation
+    2. Remove all stopwords
+    3. Returns a list of the cleaned text
+    """
+    # Check characters to see if they are in punctuation
+    print(mess)
+    nopunc = [char for char in mess if char not in string.punctuation]
+# Join the characters again to form the string.
+    nopunc = ''.join(nopunc)
+    
+    # Now just remove any stopwords
+    return " ".join([word for word in nopunc.split() if word.lower() not in stop])
+
+
+
