c78dc432f1d6e4cdead51e82dbafcbfd393c160e
megancx <55605051+megancx@users.noreply.github.com>
2021-02-06 01:02:41 -0500

    onboarding

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="container mt-5">
     <b-navbar toggleable="lg" type="light" variant="faded" id="navbar">
-     
+      
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav class="ml-auto">
           <b-nav-item class="nav-item" right>
-            <router-link class="nav-link" to="/landing"> Home </router-link>
+            <router-link class="nav-link" to="/"> Home </router-link>
           </b-nav-item>
           <b-nav-item class="nav-item" right>
             <router-link class="nav-link" to="/onboarding"> Let's Eat! </router-link>
@@ -34,12 +34,6 @@ export default {
 </script>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Kufam:wght@700&display=swap');
-h1 {
-  font-size: 4rem;
-  font-family: 'Kufam', cursive;
-  color: #2a2a2a;
-}
 #navbar {
   background: #ffffff;
   font-family: Avenir;
@@ -54,5 +48,15 @@ b-navbar{
 .nav-link {
   text-decoration: none;
   color: #000;
+}
+.btn {
+  background: #a2dce7;
+  border-color: #a2dce7;
+  color: #2a2a2a;
+}
+.btn:hover {
+  background: #32afc7;
+  color: #2a2a2a;
+  border-color: #32afc7;
 }
 </style>
